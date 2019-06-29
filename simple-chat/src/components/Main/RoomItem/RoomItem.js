e7bff01f6e5545dee19c03ffbd3291d58c628226
mango906 <minkb01@dgsw.hs.kr>
2019-06-29 11:24:27 +0900

    [ADD] Chat preview & fix file ui

--- simple-chat/src/components/Main/RoomItem/RoomItem.js
+++ simple-chat/src/components/Main/RoomItem/RoomItem.js
@@ -2,6 +2,23 @@ import React from 'react';
 import './RoomItem.scss';
 
 const RoomItem = ({ data, handleClick }) => {
+  const getType = () => {
+    const content = data.data;
+    if (content.length === 0) {
+      return null;
+    }
+
+    switch (content[content.length - 1].mimeType) {
+      case 'text':
+        return content[content.length - 1].content;
+      case 'image/jpeg':
+      case 'image/png':
+        return '(사진)';
+      default:
+        return '(파일)';
+    }
+  };
+
   return (
     <div
       className="chatitem"
@@ -12,7 +29,7 @@ const RoomItem = ({ data, handleClick }) => {
       <img className="chatitem-image" src={data.profile_image} alt="img" />
       <div className="chatitem-desc">
         <div className="chatitem-desc-name">{data.name}</div>
-        <div className="chatitem-desc-content">콘텐츠</div>
+        <div className="chatitem-desc-content">{getType()}</div>
       </div>
     </div>
   );
